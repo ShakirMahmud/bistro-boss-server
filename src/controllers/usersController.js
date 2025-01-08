160aadb4780c4bf081846193884e3fb43c721f7e
ShakirMahmud <shakirmahmud50@gmail.com>
2025-01-08 19:44:17 +0600

    Logout unauthorized access and check is admin

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,11 +3,30 @@ const { getDatabase } = require("../config/dbConnection");
 
 const usersCollection = getDatabase().collection("users");
 const getAllUsers = async (req, res) => {
+  // console.log(req.headers);
   try {
     const result = await usersCollection.find().toArray();
     res.json(result);
   } catch (error) {
     res.status(500).json({ message: "Error fetching users", error });
+  }
+};
+
+const getAdmins = async (req, res) => {
+  try {
+    const email = req.params.email;
+    if (email !== req.decoded.email) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+    const query = { email: email };
+    const result = await usersCollection.findOne(query);
+    let isAdmin = false;
+    if (result?.role === "admin") {
+      isAdmin = true;
+    }
+    res.json({ isAdmin });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching admins", error });
   }
 };
 
@@ -17,7 +36,7 @@ const postUsers = async (req, res) => {
     // do not add user if it already exists
     const existingUser = await usersCollection.findOne({ email: user.email });
     if (existingUser) {
-        return res.json({ message: "User already exists" });
+      return res.json({ message: "User already exists" });
     }
     const result = await usersCollection.insertOne(user);
     res.json(result);
@@ -37,7 +56,7 @@ const deleteUser = async (req, res) => {
   }
 };
 
-// make a user as admin 
+// make a user as admin
 const makeAdmin = async (req, res) => {
   try {
     const id = req.params.id;
@@ -63,4 +82,4 @@ const makeUser = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsers, postUsers, deleteUser, makeAdmin, makeUser };
+module.exports = { getAllUsers, postUsers, deleteUser, makeAdmin, makeUser, getAdmins };
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { getAllUsers, postUsers, deleteUser, makeAdmin, makeUser } = require('../controllers/usersController');
+const { getAllUsers, postUsers, deleteUser, makeAdmin, makeUser, getAdmins } = require('../controllers/usersController');
+const { verifyToken } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
+router.get('/', verifyToken, getAllUsers);
 router.post('/', postUsers);
 router.delete('/:id', deleteUser);
 router.patch('/admin/:id', makeAdmin);
 router.patch('/user/:id', makeUser);
+router.get('/admin/:email',verifyToken, getAdmins);
 
 module.exports = router;
