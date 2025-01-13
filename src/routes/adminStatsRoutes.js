2b1cbbb097e4f9316dd23ee2695fa9432c9bf1d8
ShakirMahmud <shakirmahmud50@gmail.com>
2025-01-13 17:30:15 +0600

    Get Order Quantity and Revenue by Category

--- src/routes/adminStatsRoutes.js
+++ src/routes/adminStatsRoutes.js
@@ -4,7 +4,7 @@ const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.get('/', adminStats);
-router.get('/orderStats',  orderStats);
+router.get('/', verifyToken, verifyAdmin, adminStats);
+router.get('/orderStats', verifyToken, verifyAdmin,  orderStats);
 
 module.exports = router;
